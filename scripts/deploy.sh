3463dc14eb8399d6741225385ce857f86a8d055a
sk1737030 <sk1737030@naver.com>
2020-05-15 15:03:57 +0900

    오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step3
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springBoot-webservice
 
 echo "> Build 파일 복사"
@@ -37,7 +37,6 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
 
 # nohup실행시 codeDeploy는 무한 대기한다.
 # 이 이슈를 해결하기위해 nohup.out 파일을 표준 입출력용으로 별도로 사용한다.
