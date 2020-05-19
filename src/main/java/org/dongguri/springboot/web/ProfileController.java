70058ed38c3f78703c2969c6037840bc6537012e
sk1737030 <sk1737030@naver.com>
2020-05-19 13:45:23 +0900

    오타수정

--- src/main/java/org/dongguri/springboot/web/ProfileController.java
+++ src/main/java/org/dongguri/springboot/web/ProfileController.java
@@ -19,6 +19,8 @@ public class ProfileController {
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
 
+        System.out.println("defaultProfile: " + defaultProfile);
+
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
